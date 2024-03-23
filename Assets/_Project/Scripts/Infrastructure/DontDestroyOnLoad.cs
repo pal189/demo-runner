7907f599386d9959206b4b505722468e71685d05
Pavel Pyatakov <paltadious@gmail.com>
2024-03-23 15:48:33 +0300

    Input prefab: moved to 'Bootstrap' scene

---
+++ Assets/_Project/Scripts/Infrastructure/DontDestroyOnLoad.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.Infrastructure
+{
+    public class DontDestroyOnLoad : MonoBehaviour
+    {
+        private void Awake() => 
+            DontDestroyOnLoad(this);
+    }
+}
